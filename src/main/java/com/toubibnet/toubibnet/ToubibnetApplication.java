f42a29b1a3f1eaecc051154dab8318ee4a2bb43d
nizarnasraoui9 <nizarnasraoui9@gmail.com>
2022-02-23 10:43:29 +0100

    first

---
+++ src/main/java/com/toubibnet/toubibnet/ToubibnetApplication.java
@@ -0,0 +1,13 @@
+package com.toubibnet.toubibnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToubibnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToubibnetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toubibnet/toubibnet/ToubibnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toubibnet.toubibnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToubibnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
