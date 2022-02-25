f74d8130b6de97bb6eedd3f5c412256f9a1f0d6a
Siwar Boughdir <siwarboughdir134@gmail.com>
2022-02-25 01:06:09 +0100

    commit answer modification

--- src/main/java/com/toubibnet/toubibnet/repository/DoctorRepo.java
+++ src/main/java/com/toubibnet/toubibnet/repository/DoctorRepo.java
@@ -1,7 +1,10 @@
 package com.toubibnet.toubibnet.repository;
 
+import com.toubibnet.toubibnet.model.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public interface DoctorRepo extends UserRepo {
+public interface DoctorRepo extends JpaRepository<Doctor, Long> {
 }
--- src/main/java/com/toubibnet/toubibnet/service/AnswerService.java
+++ src/main/java/com/toubibnet/toubibnet/service/AnswerService.java
@@ -1,10 +1,17 @@
 package com.toubibnet.toubibnet.service;
 
 import com.toubibnet.toubibnet.model.Answer;
+import com.toubibnet.toubibnet.model.Doctor;
+import com.toubibnet.toubibnet.model.Question;
+import com.toubibnet.toubibnet.model.User;
 import com.toubibnet.toubibnet.repository.AnswerRepository;
+import com.toubibnet.toubibnet.repository.DoctorRepo;
+import com.toubibnet.toubibnet.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.print.Doc;
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +19,12 @@ import java.util.Optional;
 public class AnswerService {
     @Autowired
     AnswerRepository answerRepository;
+
+    @Autowired
+    QuestionRepository questionRepository;
+
+    @Autowired
+    DoctorRepo doctorRepo;
 
     public List<Answer> findAll() {
         return answerRepository.findAll();
@@ -24,17 +37,29 @@ public class AnswerService {
         }
         return answer.get();
     }
+    public List<Answer> findByQuestion(Long id) {
+        List<Answer> answer = answerRepository.findByQuestion(id);
+        return answer;
+    }
 
-    public Answer save(Answer answer) {
+    public Answer save(Answer answer, Long idQuestion, Long idDoctor) {
+        Optional<Question> question = questionRepository.findById(idQuestion);
+        Optional<Doctor> doctor = doctorRepo.findById(idDoctor);
+        answer.setDoctor(doctor.get());
+        answer.setQuestion(question.get());
         return answerRepository.save(answer);
     }
 
     public Answer update(Answer answer) {
         Optional<Answer> answer1 = answerRepository.findById(answer.getId());
-        if (!answer1.isPresent()) {
+        if (!answer1.isPresent()){
+            System.err.println("answer null");
             return new Answer();
         }
-        return answerRepository.saveAndFlush(answer);
+        System.err.println(answer1);
+        answer.setDoctor(answer1.get().getDoctor());
+        answer.setQuestion(answer1.get().getQuestion());
+        return answerRepository.save(answer);
     }
 
     public boolean delete(Long id) {
