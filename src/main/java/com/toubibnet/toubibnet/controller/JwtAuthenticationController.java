ab51019a33cf6794c29f6dc261567918f72c75f3
AchrefTrabelsi <achref.trabelsi@ensi-uma.tn>
2022-02-25 17:12:34 +0100

    added admin login

--- src/main/java/com/toubibnet/toubibnet/controller/JwtAuthenticationController.java
+++ src/main/java/com/toubibnet/toubibnet/controller/JwtAuthenticationController.java
@@ -19,6 +19,8 @@ import com.toubibnet.toubibnet.exception.UserNotFoundException;
 import com.toubibnet.toubibnet.model.JwtResponse;
 import com.toubibnet.toubibnet.model.JwtSignInRequest;
 import com.toubibnet.toubibnet.model.JwtUserSignUpRequest;
+import com.toubibnet.toubibnet.model.Role;
+import com.toubibnet.toubibnet.repository.UserRepo;
 import com.toubibnet.toubibnet.service.AuthenticationService;
 
 @CrossOrigin
@@ -33,6 +35,8 @@ public class JwtAuthenticationController {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
+	@Autowired
+	private UserRepo userRepository;
 	
 
 	@RequestMapping(value = "/auth/signin", method = RequestMethod.POST)
@@ -50,6 +54,26 @@ public class JwtAuthenticationController {
 		}
 		
 	}
+	@RequestMapping(value = "/auth/signin/admin", method = RequestMethod.POST)
+	public ResponseEntity<JwtResponse> createAuthenticationTokenForAdmin(@RequestBody JwtSignInRequest authenticationRequest) throws Exception{
+
+		authenticationService.authenticate(authenticationRequest);
+		
+		try {
+			final UserDetails userDetails = userDetailsService
+					.loadUserByUsername(authenticationRequest.getEmail());
+			final String token = jwtTokenUtil.generateToken(userDetails);
+			boolean isAdmin =this.userRepository.findByEmail(userDetails.getUsername()).
+			getRoles().stream().anyMatch((Role r)-> r.getName().equals(Role.ADMIN));
+			if(!isAdmin)
+				return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+			return new ResponseEntity<JwtResponse>(new JwtResponse(token,userDetails), HttpStatus.OK);
+		}catch(UserNotFoundException e) {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+		
+	}
+
 	
 	@RequestMapping(value = "/auth/signup/user", method = RequestMethod.POST)
 	public void signUpUser(@RequestBody JwtUserSignUpRequest authenticationRequest) {
--- src/main/java/com/toubibnet/toubibnet/repository/UserRepo.java
+++ src/main/java/com/toubibnet/toubibnet/repository/UserRepo.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepo extends JpaRepository<User,Long> {
-	User findByEmail(String email);
+	public User findByEmail(String email);
 }
