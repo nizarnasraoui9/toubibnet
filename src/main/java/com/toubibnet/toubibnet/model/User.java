b49621bf5e3da51766a58058ae40df1868aa0665
AchrefTrabelsi <achref.trabelsi@ensi-uma.tn>
2022-02-28 09:14:48 +0100

    commit

--- src/main/java/com/toubibnet/toubibnet/model/User.java
+++ src/main/java/com/toubibnet/toubibnet/model/User.java
@@ -2,7 +2,6 @@ package com.toubibnet.toubibnet.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
