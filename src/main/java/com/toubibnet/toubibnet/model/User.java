200925816dda059bf9457604b7866777f5c1ca2f
AchrefTrabelsi <achref.trabelsi@ensi-uma.tn>
2022-02-28 09:10:38 +0100

    minor bug

--- src/main/java/com/toubibnet/toubibnet/model/User.java
+++ src/main/java/com/toubibnet/toubibnet/model/User.java
@@ -3,7 +3,9 @@ package com.toubibnet.toubibnet.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
@@ -19,7 +21,8 @@ import static javax.persistence.FetchType.EAGER;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Setter
+@Getter
 @Inheritance(strategy = InheritanceType.JOINED)
 public class User implements UserDetails {
     
@@ -83,4 +86,5 @@ public class User implements UserDetails {
 		return true;
 
 	}
+	
 }
