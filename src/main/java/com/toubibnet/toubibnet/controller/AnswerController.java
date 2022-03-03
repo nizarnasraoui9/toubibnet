f0e5f80e13273160df4b4b88c7b19e62edc4f936
Saifallah El Mouelhi <saifallah.elmouelhi@etudiant-fst.utm.tn>
2022-03-03 11:17:12 +0100

    commit question Pageable

--- src/main/java/com/toubibnet/toubibnet/controller/AnswerController.java
+++ src/main/java/com/toubibnet/toubibnet/controller/AnswerController.java
@@ -2,6 +2,8 @@ package com.toubibnet.toubibnet.controller;
 
 import com.toubibnet.toubibnet.model.Answer;
 import com.toubibnet.toubibnet.service.AnswerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,36 +13,43 @@ import java.util.List;
 @RequestMapping("/answer")
 @RestController
 public class AnswerController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(QuestionController.class);
     @Autowired
     AnswerService answerService;
 
 
     @GetMapping
     public List<Answer> findAll() {
+        LOGGER.info("getAllAnswer");
         return answerService.findAll();
     }
 
     @GetMapping("/{id}")
     public Answer findById(@PathVariable Long id) {
+        LOGGER.info("getAnswerById= " + id);
         return answerService.findById(id);
     }
 
     @GetMapping("/question/{id}")
     public List<Answer> findByQuestion(@PathVariable Long id){
+        LOGGER.info("getAllAnswersForQuestionId= " + id);
         return answerService.findByQuestion(id);
     }
     @PostMapping("/question/{idQuestion}/doctor/{idDoctor}")
     public Answer save(@RequestBody Answer answer, @PathVariable Long idQuestion, @PathVariable Long idDoctor) {
+        LOGGER.info("saveAnswerforQuestionId= " + idQuestion + " ByDoctorId=" + idDoctor + " AndAnswer=" + answer.toString());
         return answerService.save(answer, idQuestion, idDoctor);
     }
 
     @PutMapping
     public Answer update(@RequestBody Answer answer) {
+        LOGGER.info("UpdateAnswer= " + answer.toString());
         return answerService.update(answer);
     }
 
     @DeleteMapping("/{id}")
     public boolean delete(@PathVariable Long id) {
+        LOGGER.info("deleteAnswerById= " + id);
         return answerService.delete(id);
     }
 
--- src/main/java/com/toubibnet/toubibnet/controller/QuestionController.java
+++ src/main/java/com/toubibnet/toubibnet/controller/QuestionController.java
@@ -21,44 +21,94 @@ public class QuestionController {
 
     @GetMapping
     public List<Question> findAll() {
-        LOGGER.info("getAll");
+        LOGGER.info("getAllQuestion");
         return questionService.findAll();
+    }
+
+    @GetMapping(value = "/page/{page}/{size}")
+    public List<Question> findAllPerPage(@PathVariable(value = "page") Integer page,
+                                         @PathVariable(value = "size") Integer size) {
+        LOGGER.info("getAllQuestion");
+        return questionService.findAllPerPage(page, size);
+    }
+
+    @GetMapping(value = "/totalelements/{page}/{size}")
+    public Long findNumberOfTotalElement(@PathVariable(value = "page") Integer page,
+                                         @PathVariable(value = "size") Integer size) {
+        LOGGER.info("getAllQuestion");
+        return questionService.findNumberOfTotalElement(page, size);
     }
 
     @GetMapping("/{id}")
     public Question findById(@PathVariable(value = "id") Long id) {
+        LOGGER.info("getQuestionByid/" + id);
         return questionService.findById(id);
-
     }
 
     @GetMapping("/category/{category}")
     public List<Question> findByCategory(@PathVariable(value = "category") Category category) {
+        LOGGER.info("getQuestionByCategory/" + category);
         return questionService.findByCategory(category);
-
     }
+
+    @GetMapping("/category/{category}/page/{page}/{size}")
+    public List<Question> findByCategoryPerPage(@PathVariable(value = "category") Category category,
+                                                @PathVariable(value = "page") Integer page,
+                                                @PathVariable(value = "size") Integer size) {
+        LOGGER.info("getQuestionByCategory/" + category);
+        return questionService.findByCategoryPerPage(category, page, size);
+    }
+
+    @GetMapping("/totalelementsbycategory/{category}/page/{page}/{size}")
+    public Long findNumberOfTotalElementByCategory(@PathVariable(value = "category") Category category,
+                                                   @PathVariable(value = "page") Integer page,
+                                                   @PathVariable(value = "size") Integer size) {
+        LOGGER.info("getQuestionByCategory/" + category);
+        return questionService.findNumberOfTotalElementByCategory(category, page, size);
+    }
+
     @GetMapping("/search/{mot}")
     public List<Question> findByWord(@PathVariable(value = "mot") String mot) {
+        LOGGER.info("getQuestionByWord/" + mot);
         return questionService.findByWord(mot);
-
     }
+
+    @GetMapping("/search/{mot}/page/{size}/{page}")
+    public List<Question> findByWordPerPage(@PathVariable(value = "mot") String mot,
+                                            @PathVariable(value = "page") Integer page,
+                                            @PathVariable(value = "size") Integer size) {
+        LOGGER.info("getQuestionByWord/" + mot);
+        return questionService.findByWordPerPage(mot, page, size);
+    }
+
+    @GetMapping("/totalelementsbyword/{mot}/page/{size}/{page}")
+    public Long findNumberOfElementByWord(@PathVariable(value = "mot") String mot,
+                                          @PathVariable(value = "page") Integer page,
+                                          @PathVariable(value = "size") Integer size) {
+        LOGGER.info("getQuestionByWord/" + mot);
+        return questionService.findNumberOfElementByWord(mot, page, size);
+    }
+
     @GetMapping("/categories")
     public List<String> findByCategories() {
         return questionService.findCategories();
-
     }
 
     @PostMapping("/{id}")
     public Question save(@PathVariable(value = "id") Long id, @RequestBody Question question) {
+        LOGGER.info("saveQuestionByUser/" + id + " andquetionId= " + question.getId());
         return questionService.save(question, id);
     }
 
     @PutMapping
     public Question update(@RequestBody Question question) {
+        LOGGER.info("updateQuestion= " + question.getId());
         return questionService.update(question);
     }
 
     @DeleteMapping("/{id}")
     public boolean delete(@PathVariable(value = "id") Long id) {
+        LOGGER.info("deleteQuestionById= " + id);
         return questionService.deleteById(id);
     }
 
